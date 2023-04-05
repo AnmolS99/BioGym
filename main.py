e8b07a662b7e079af4ea68bf375ed20507bdea65
Anmol Singh <Anmol_singh@live.no>
2023-04-05 13:11:40 +0200

    Results DQN_2x2_10x

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from agents.user_action import UserAction
 
 np.set_printoptions(suppress=True, formatter={'float': "{0:0.3f}".format})
 
-config_parser = ConfigParser("bio_env_configs/default.ini")
+config_parser = ConfigParser("bio_env_configs/2x2_10x.ini")
 env = config_parser.create_bio_gym_world()
 
-model_type = PPO
+model_type = DQN
 
 
-def train_model(model_name):
+def train_model(model_name, timesteps):
     env.renderer.render_mode = "off"
     model = model_type("MultiInputPolicy",
                        env,
                        verbose=1,
                        tensorboard_log="./logs/")
-    model.learn(total_timesteps=200_000,
+    model.learn(total_timesteps=timesteps,
                 progress_bar=True,
                 tb_log_name=model_name)
     model.save("trained_models/" + model_name)
@@ -94,11 +94,13 @@ def run(episodes,
 
 
 if __name__ == '__main__':
-    model_name = "PPO_200k_harvest_rew"
+    for i in range(1, 11):
 
-    # train_model(model_name)
-    run(episodes=2,
-        render_mode="on",
-        show_episode_history=False,
-        agent_name="model",
-        model_name=model_name)
+        model_name = "DQN_2x2_10x_200k_" + str(i)
+        train_model(model_name, 200_000)
+
+    # run(episodes=2,
+    #     render_mode="on",
+    #     show_episode_history=False,
+    #     agent_name="model",
+    #     model_name=model_name)
