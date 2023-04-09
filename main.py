24b1c71229e73b7b6e2bf2eaed4ba748a6ca9835
Anmol Singh <Anmol_singh@live.no>
2023-04-09 14:27:19 +0200

    Results DQN_4x4_10x

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from agents.user_action import UserAction
 
 np.set_printoptions(suppress=True, formatter={'float': "{0:0.3f}".format})
 
-config_parser = ConfigParser("bio_env_configs/3x3_10x.ini")
+config_parser = ConfigParser("bio_env_configs/4x4_10x.ini")
 env = config_parser.create_bio_gym_world()
 
-model_type = PPO
+model_type = DQN
 
 
 def train_model(model_name, timesteps):
@@ -96,7 +96,7 @@ def run(episodes,
 if __name__ == '__main__':
     for i in range(1, 21):
 
-        model_name = "PPO_3x3_10x_200k_" + str(i)
+        model_name = "DQN_4x4_10x_200k_" + str(i)
         train_model(model_name, 200_000)
 
     # run(episodes=2,
